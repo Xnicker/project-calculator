fc58b8ff285e91cd9f51f734cc0489b592eb002f
Tim <xniicga@gmail.com>
2021-08-03 18:01:50 +0200

    Buttons im Array angeordnet. Aktuell keine andere Lösung. Klassenzuweisung der Divs und Buttons abhängig von der zugewiesenen Taste

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-const calculatorbox = document.querySelector("#calculatorbox");
+const calculatorbox = document.querySelector("#calcBody");
 const btnMapping = /*Enthält alle Buttons für den Calculator */[
     {
-        taste:"0",
-        dataKey:96,
-        info:"0",
+        taste:"AC",
+        dataKey:89,
+        info:"zuruecksetzen",
     },
-
     {
-        taste: "1",
-        dataKey:97,
-        info:"1", 
+        taste:"C",
+        dataKey:67,
+        info:"loeschen",
     },
     
     {
-        taste: "2",
-        dataKey:98,
-        info:"2",
+        taste:"wrz",
+        dataKey:87,
+        info:"wurzel",
+    },
+    {
+        taste:"prz",
+        dataKey:80,
+        info:"prozent",
+    },
+    {
+        taste:"7",
+        dataKey:103,
+        info:"7",
+    },
+        
+    {
+        taste:"8",
+        dataKey:104,
+        info:"8",
+    },
+        
+    {
+        taste:"9",
+        dataKey:105,
+        info:"9",
     },
 
     {
-        taste:"3",
-        dataKey:99,
-        info:"3",
+        taste:"/",
+        dataKey:111,
+        info:"getelt",
     },
 
     {
@@ -41,25 +62,46 @@ const btnMapping = /*Enthält alle Buttons für den Calculator */[
         dataKey:102,
         info:"6",
     },
+    {
+        taste:"x",
+        dataKey:106,
+        info:"mal",
+    },
+    {
+        taste: "1",
+        dataKey:97,
+        info:"1", 
+    },
+    
+    {
+        taste: "2",
+        dataKey:98,
+        info:"2",
+    },
 
     {
-        taste:"7",
-        dataKey:103,
-        info:"7",
+        taste:"3",
+        dataKey:99,
+        info:"3",
     },
-        
+
     {
-        taste:"8",
-        dataKey:104,
-        info:"8",
+        taste:"-",
+        dataKey:109,
+        info:"minus",
     },
-        
+
     {
-        taste:"9",
-        dataKey:105,
-        info:"9",
+        taste: "0",
+        dataKey:48,
+        info:"0", 
     },
-        
+        {
+        taste: ".",
+        dataKey:190,
+        info:".", 
+    },
+    
     {
         taste:"=",
         dataKey:48,
@@ -72,48 +114,25 @@ const btnMapping = /*Enthält alle Buttons für den Calculator */[
         info:"plus",
     },
     
-    {
-        taste:"-",
-        dataKey:109,
-        info:"minus",
-    },
-    {
-        taste:"x",
-        dataKey:106,
-        info:"mal",
-    },
-    {
-        taste:"/",
-        dataKey:111,
-        info:"getelt",
-    },
-    {
-        taste:"C",
-        dataKey:67,
-        info:"loeschen",
-    },
-    {
-        taste:"AC",
-        dataKey:89,
-        info:"zuruecksetzen",
-    },
-    {
-        taste:"wrz",
-        dataKey:87,
-        info:"wurzel",
-    },
-    {
-        taste:"prz",
-        dataKey:80,
-        info:"prozent",
-    },
+    
+    
+    
 ]
+const regexZahlen = /\d/;
 btnMapping.forEach(function(button){ /* Initialisieren der zugehörigen divs und buttons */
     const divButton = document.createElement("div");
     const clickButton = document.createElement("button");
-    divButton.classList.add("calccontainer");
+    /*unelegante Lösung, Regex muss noch zum laufen gebracht werden.
+     Damit die Buttons abhängig vom "Typ" die richtige Klasse kriegen  */
+    if (button.taste >= 0) {  
+        divButton.classList.add("operatorDiv");
+        clickButton.classList.add("operatorButton");
+    }
+    else {
+        divButton.classList.add("operandDiv");
+        clickButton.classList.add("operandButton");
+    }
     divButton.id=`divButton${button.info}`;
-    clickButton.classList.add("buttonCalc");
     clickButton.id=`button${button.info}`;
     clickButton.dataKey=button.dataKey;
     clickButton.textContent=button.taste;
